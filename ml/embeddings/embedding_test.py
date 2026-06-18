3b2a9d9ea59780f3537c9429018daada3c3554b2
Nitish priyan R S <nitishpriyan1807@gmail.com>
2026-06-18 15:41:01 +0000

    tested semantic similarity between payment related texts

--- ml/embeddings/embedding_test.py
+++ ml/embeddings/embedding_test.py
@@ -1,9 +1,14 @@
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-text = "transaction successful amazon pay"
+text1 = "upi payment receipt"
+text2 = "upi transaction payment successful"
 
-embedding = model.encode(text)
+emb1 = model.encode([text1])
+emb2 = model.encode([text2])
 
-print("Embedding Length:", len(embedding))
+score = cosine_similarity(emb1, emb2)
+
+print(score)
