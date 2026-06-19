2e0bc5fc70b1ed76430fe49b817b5af7c7616887
Nitish priyan R S <nitishpriyan1807@gmail.com>
2026-06-19 15:17:59 +0000

    added sample screenshot corpus for retrieval testing

---
+++ ml/retrieval/search_test.py
@@ -0,0 +1,7 @@
+screenshots = [
+    "transaction successful amazon pay",
+    "irctc train ticket chennai",
+    "python index error list out of range",
+    "amazon order delivered",
+    "college operating systems notes"
+]
