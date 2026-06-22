a28598a07a1cad586005febfab826c79520cca36
Nitish priyan R S <nitishpriyan1807@gmail.com>
2026-06-22 16:00:21 +0000

    added screenshot corpus inside backend service

--- backend/app.py
+++ backend/app.py
@@ -14,5 +14,7 @@ def search():
 screenshots = [
     "transaction successful amazon pay",
     "irctc train ticket chennai",
-
+    "python index error list out of range",
+    "amazon order delivered",
+    "college operating systems notes"
 ]
