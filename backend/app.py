b68fc15d5de99eeabce1e49e8d90a6901f237be9
Nitish priyan R S <nitishpriyan1807@gmail.com>
2026-06-23 15:22:30 +0000

    implemented semantic search function in backend

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,11 @@
 from sentence_transformers import SentenceTransformer
 from fastapi import FastAPI
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 app = FastAPI()
-model = SentenceTransformer('all-MiniLM-L6-v2')
-@app.get("/")
-def home():
-    return {"message": "SnapMind AI backend running"}
 
-@app.get("/search")
-def search():
-    return {"query": "amazon payment", "result": "p1.png"}
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 screenshots = [
     "transaction successful amazon pay",
@@ -18,3 +14,29 @@ screenshots = [
     "amazon order delivered",
     "college operating systems notes"
 ]
+
+embeddings = model.encode(screenshots)
+
+def semantic_search(query):
+    query_embedding = model.encode([query])
+
+    scores = cosine_similarity(query_embedding, embeddings)
+
+    best_match = np.argmax(scores)
+
+    return screenshots[best_match]
+
+@app.get("/")
+def home():
+    return {"message": "SnapMind AI backend running"}
+
+@app.get("/search")
+def search():
+    query = "amazon payment"
+
+    result = semantic_search(query)
+
+    return {
+        "query": query,
+        "result": result
+    }
